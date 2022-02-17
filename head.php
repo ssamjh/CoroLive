33f61aebd8b61eedd8aaa041aec797bb75f244e0
ssamjh <sam.howell.sjh@gmail.com>
2022-02-17 23:59:20 +1300

    Restore favicon

--- head.php
+++ head.php
@@ -12,3 +12,5 @@
 </script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+<link rel="icon" type="image/webp" href="img/corolive64.webp">

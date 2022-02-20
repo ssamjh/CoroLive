16908c6f5c14803d6f9da5a48df50ed3117a879c
ssamjh <sam.howell.sjh@gmail.com>
2022-02-20 23:54:39 +1300

    Switch imgproxy to API domain

--- index.php
+++ index.php
@@ -23,8 +23,8 @@
                 <div class="card text-center">
                     <div class="card-body">
                         <a href="whitianga"><img id="whitianga-thumbnail"
-                                src="https://api.corolive.nz/whitianga/snap.webp" draggable="false" class="card-img-top"
-                                alt="Whitianga picker image"></a>
+                                src="https://api.corolive.nz/imgproxy/S5fvR7DSxAnpNu1Os0xd37tAiFNTAR-l1bpoqPvPTRY/rs:fit::360:0/g:no/aHR0cHM6Ly9hcGkuY29yb2xpdmUubnovd2hpdGlhbmdhL3NuYXAud2VicA.webp"
+                                draggable="false" class="card-img-top" alt="Whitianga picker image"></a>
                         <h5 class="card-title text-center">Whitianga</h5>
                         <div class="center">
                             <a href="whitianga" class="btn btn-primary">Live</a>
@@ -39,8 +39,8 @@
                 <div class="card text-center">
                     <div class="card-body">
                         <a href="whangamata"><img id="whangamata-thumbnail"
-                                src="https://api.corolive.nz/whangamata/snap.webp" draggable="false"
-                                class="card-img-top" alt="Whangamata picker image"></a>
+                                src="https://api.corolive.nz/imgproxy/9nD2ByfvIPnevn470C9-JJ6lt62L7STOasLq6QMsFak/rs:fit::360:0/g:no/aHR0cHM6Ly9hcGkuY29yb2xpdmUubnovd2hhbmdhbWF0YS9zbmFwLndlYnA.webp"
+                                draggable="false" class="card-img-top" alt="Whangamata picker image"></a>
                         <h5 class="card-title text-center">Whangamata</h5>
                         <div class="center">
                             <a href="whangamata" class="btn btn-primary">Live</a>
@@ -54,7 +54,8 @@
             <div class="col-sm-3">
                 <div class="card text-center">
                     <div class="card-body">
-                        <a href="thames"><img id="thames-thumbnail" src="https://api.corolive.nz/thames/snap.webp"
+                        <a href="thames"><img id="thames-thumbnail"
+                                src="https://api.corolive.nz/imgproxy/e7DkeRUycsd3to1M7TU2yHxBWK-GvvAiMus8jWNBmDM/rs:fit::360:0/g:no/aHR0cHM6Ly9hcGkuY29yb2xpdmUubnovdGhhbWVzL3NuYXAud2VicA.webp"
                                 draggable="false" class="card-img-top" alt="Thames picker image"></a>
                         <h5 class="card-title text-center">Thames</h5>
                         <div class="center">
@@ -72,13 +73,19 @@
     <script>
     setInterval(function() {
         var whitiangaThumbnail = document.getElementById('whitianga-thumbnail');
-        whitiangaThumbnail.src = 'https://api.corolive.nz/whitianga/snap.webp?rand=' + Math.random();
+        whitiangaThumbnail.src =
+            'https://api.corolive.nz/imgproxy/S5fvR7DSxAnpNu1Os0xd37tAiFNTAR-l1bpoqPvPTRY/rs:fit::360:0/g:no/aHR0cHM6Ly9hcGkuY29yb2xpdmUubnovd2hpdGlhbmdhL3NuYXAud2VicA.webp?rand=' +
+            Math.random();
 
         var whangamataThumbnail = document.getElementById('whangamata-thumbnail');
-        whangamataThumbnail.src = 'https://api.corolive.nz/whangamata/snap.webp?rand=' + Math.random();
+        whangamataThumbnail.src =
+            'https://api.corolive.nz/imgproxy/9nD2ByfvIPnevn470C9-JJ6lt62L7STOasLq6QMsFak/rs:fit::360:0/g:no/aHR0cHM6Ly9hcGkuY29yb2xpdmUubnovd2hhbmdhbWF0YS9zbmFwLndlYnA.webp?rand=' +
+            Math.random();
 
         var thamesThumbnail = document.getElementById('thames-thumbnail');
-        thamesThumbnail.src = 'https://api.corolive.nz/thames/snap.webp?rand=' + Math.random();
+        thamesThumbnail.src =
+            'https://api.corolive.nz/imgproxy/e7DkeRUycsd3to1M7TU2yHxBWK-GvvAiMus8jWNBmDM/rs:fit::360:0/g:no/aHR0cHM6Ly9hcGkuY29yb2xpdmUubnovdGhhbWVzL3NuYXAud2VicA.webp?rand=' +
+            Math.random();
     }, 15000);
     </script>
 

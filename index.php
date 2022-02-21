a48582eee51531fc51d643ad2938f09d0583a5b2
ssamjh <sam.howell.sjh@gmail.com>
2022-02-21 21:21:55 +1300

    Make snap URL rand on first load

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
                 <div class="card text-center">
                     <div class="card-body">
                         <a href="whitianga"><img id="whitianga-thumbnail"
-                                src="https://api.corolive.nz/imgproxy/S5fvR7DSxAnpNu1Os0xd37tAiFNTAR-l1bpoqPvPTRY/rs:fit::360:0/g:no/aHR0cHM6Ly9hcGkuY29yb2xpdmUubnovd2hpdGlhbmdhL3NuYXAud2VicA.webp"
+                                src="https://api.corolive.nz/imgproxy/S5fvR7DSxAnpNu1Os0xd37tAiFNTAR-l1bpoqPvPTRY/rs:fit::360:0/g:no/aHR0cHM6Ly9hcGkuY29yb2xpdmUubnovd2hpdGlhbmdhL3NuYXAud2VicA.webp?rand=<?php echo (rand());?>"
                                 draggable="false" class="card-img-top" alt="Whitianga picker image"></a>
                         <h5 class="card-title text-center">Whitianga</h5>
                         <div class="center">
@@ -39,7 +39,7 @@
                 <div class="card text-center">
                     <div class="card-body">
                         <a href="whangamata"><img id="whangamata-thumbnail"
-                                src="https://api.corolive.nz/imgproxy/9nD2ByfvIPnevn470C9-JJ6lt62L7STOasLq6QMsFak/rs:fit::360:0/g:no/aHR0cHM6Ly9hcGkuY29yb2xpdmUubnovd2hhbmdhbWF0YS9zbmFwLndlYnA.webp"
+                                src="https://api.corolive.nz/imgproxy/9nD2ByfvIPnevn470C9-JJ6lt62L7STOasLq6QMsFak/rs:fit::360:0/g:no/aHR0cHM6Ly9hcGkuY29yb2xpdmUubnovd2hhbmdhbWF0YS9zbmFwLndlYnA.webp?rand=<?php echo (rand());?>"
                                 draggable="false" class="card-img-top" alt="Whangamata picker image"></a>
                         <h5 class="card-title text-center">Whangamata</h5>
                         <div class="center">
@@ -55,7 +55,7 @@
                 <div class="card text-center">
                     <div class="card-body">
                         <a href="thames"><img id="thames-thumbnail"
-                                src="https://api.corolive.nz/imgproxy/e7DkeRUycsd3to1M7TU2yHxBWK-GvvAiMus8jWNBmDM/rs:fit::360:0/g:no/aHR0cHM6Ly9hcGkuY29yb2xpdmUubnovdGhhbWVzL3NuYXAud2VicA.webp"
+                                src="https://api.corolive.nz/imgproxy/e7DkeRUycsd3to1M7TU2yHxBWK-GvvAiMus8jWNBmDM/rs:fit::360:0/g:no/aHR0cHM6Ly9hcGkuY29yb2xpdmUubnovdGhhbWVzL3NuYXAud2VicA.webp?rand=<?php echo (rand());?>"
                                 draggable="false" class="card-img-top" alt="Thames picker image"></a>
                         <h5 class="card-title text-center">Thames</h5>
                         <div class="center">

b956c8584a09d82ba0f989fcd26e0b7f72472036
ssamjh <sam.howell.sjh@gmail.com>
2022-02-22 22:14:53 +1300

    Center date selector

--- timelapse-picker.php
+++ timelapse-picker.php
@@ -1,18 +1,19 @@
 <br>
-<br>
-<div class="row justify-content-center">
-    <div class="col-xxl-8">
-        <form>
-            <label>
-                Or... pick another date:
-                <input type="date" class="form-control" name="date" min="<?php echo "$startDate"; ?>" max="<?php $date = new DateTime("now", new DateTimeZone('Pacific/Auckland') );
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-xxl-8">
+            <form class="text-center">
+                <label class="text-center">
+                    Or... pick another date:
+                    <input type="date" class="form-control" name="date" min="<?php echo "$startDate"; ?>" max="<?php $date = new DateTime("now", new DateTimeZone('Pacific/Auckland') );
 echo $date->format('Y-m-d');?>" value="<?php echo $_GET['date']; ?>" required>
-                <span class="validity"></span>
-            </label>
+                    <span class="validity"></span>
+                </label>
 
-            <p>
-                <button class="btn btn-secondary">Submit</button>
-            </p>
-        </form>
+                <p>
+                    <button class="btn btn-secondary text-center">Submit</button>
+                </p>
+            </form>
+        </div>
     </div>
 </div>

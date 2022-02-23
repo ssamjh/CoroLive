6936121a1ade6b064ccf01a9d9125b45d7c9cb2c
ssamjh <sam.howell.sjh@gmail.com>
2022-02-23 23:14:49 +1300

    Move animation to 2 parts

--- scripts/camanimation-p2.sh
+++ scripts/camanimation-p2.sh
@@ -1,16 +1,3 @@
-# Remove any existing folder from last animation.
-rm -rf /tmp/$1-animation
-#
-# Create a fresh new folder for this animation.
-mkdir /tmp/$1-animation
-mkdir /tmp/$1-animation/jpg
-mkdir /tmp/$1-animation/webp
-#
-# Copy all images from today to our temporary folder.
-#
-cp /var/www/html/corolive.nz/api/$1/archive/latest/*.jpg /tmp/$1-animation/jpg/
-cp /var/www/html/corolive.nz/api/$1/archive/latest/*.webp /tmp/$1-animation/webp/
-#
 # Move to the jpg folder.
 cd /tmp/$1-animation/jpg
 #

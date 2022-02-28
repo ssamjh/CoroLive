0d90b328f21fb22e38aca0e06a38e2f99a25ce58
ssamjh <sam.howell.sjh@gmail.com>
2022-03-01 11:57:59 +1300

    Remove more legacy stuff

--- scripts/camanimation-p1.sh
+++ scripts/camanimation-p1.sh
@@ -2,20 +2,11 @@
 rm -rf /tmp/$1-animation
 #
 # Create a fresh new folder for this animation.
-mkdir /tmp/$1-animation
-mkdir /tmp/$1-animation/jpg
-mkdir /tmp/$1-animation/webp
+mkdir -p /tmp/$1-animation/webp
 #
 # Copy all images from today to our temporary folder.
 #
-cp /var/www/html/corolive.nz/api/$1/archive/latest/*.jpg /tmp/$1-animation/jpg/
 cp /var/www/html/corolive.nz/api/$1/archive/latest/*.webp /tmp/$1-animation/webp/
-#
-# Move to the jpg folder.
-cd /tmp/$1-animation/jpg
-#
-# Rename all images from 1.jpg onwards. With 1.jpg being the first image.
-ls | cat -n | while read n f; do mv "$f" `printf "%d.jpg" $n`; done
 #
 # Move to the jpg folder.
 cd /tmp/$1-animation/webp

3f3b82798f2753e4569d270fbe98cf7bb47a87d1
ssamjh <sam.howell.sjh@gmail.com>
2022-03-01 11:55:48 +1300

    Remove jpg processing

--- scripts/camanimation-p2.sh
+++ scripts/camanimation-p2.sh
@@ -1,14 +1,14 @@
 # Create the normal speed animation. (crf was 28)
-cd /tmp/$1-animation/jpg
-/opt/ffmpeg-git-20220108-amd64-static/ffmpeg -r 12 -i %d.jpg -c:v libx264 -crf 30 -vf "fps=12,format=yuv420p" animation.mp4
+#cd /tmp/$1-animation/jpg
+#/opt/ffmpeg-git-20220108-amd64-static/ffmpeg -r 12 -i %d.jpg -c:v libx264 -crf 30 -vf "fps=12,format=yuv420p" animation.mp4
 #
 cd /tmp/$1-animation/webp
 /opt/ffmpeg-git-20220108-amd64-static/ffmpeg -r 12 -i %d.webp -c:v libvpx-vp9 -b:v 0 -crf 38 -deadline good -cpu-used 5 -vf "fps=12" -pass 1 -an -f null /dev/null && \
 /opt/ffmpeg-git-20220108-amd64-static/ffmpeg -r 12 -i %d.webp -c:v libvpx-vp9 -b:v 0 -crf 38 -deadline good -cpu-used 5 -vf "fps=12" -pass 2 -an animation.webm
 #
 # Create the faster speed animation.
-cd /tmp/$1-animation/jpg
-/opt/ffmpeg-git-20220108-amd64-static/ffmpeg -r 30 -i %d.jpg -c:v libx264 -crf 30 -vf "fps=30,format=yuv420p" animation-fast.mp4
+#cd /tmp/$1-animation/jpg
+#/opt/ffmpeg-git-20220108-amd64-static/ffmpeg -r 30 -i %d.jpg -c:v libx264 -crf 30 -vf "fps=30,format=yuv420p" animation-fast.mp4
 #
 cd /tmp/$1-animation/webp
 /opt/ffmpeg-git-20220108-amd64-static/ffmpeg -r 30 -i %d.webp -c:v libvpx-vp9 -b:v 0 -crf 42 -deadline good -cpu-used 5 -vf "fps=30" -pass 1 -an -f null /dev/null && \
--- scripts/camsnap.sh
+++ scripts/camsnap.sh
@@ -1,11 +1,4 @@
 # Grab a still from camera.
 curl --connect-timeout 2 --retry 4 --retry-delay 1 -s -S -f -o /tmp/$1-camsnap.jpg $2 \
-&& cp /tmp/$1-camsnap.jpg /tmp/$1-camsnap-optim.jpg \
-&& mogrify -resize 1280x720 /tmp/$1-camsnap-optim.jpg \
-&& jpegoptim --max=45 --strip-all /tmp/$1-camsnap-optim.jpg \
-&& mv /tmp/$1-camsnap-optim.jpg "/var/www/html/corolive.nz/api/$1/archive/latest/snap-$(date +%R).jpg" \
 && cwebp /tmp/$1-camsnap.jpg -quiet -preset photo -q 50 -resize 1280 720 -metadata none -o /tmp/$1-camsnap-optimised.webp \
 && mv /tmp/$1-camsnap-optimised.webp "/var/www/html/corolive.nz/api/$1/archive/latest/snap-$(date +%R).webp"
-
-
-#&& cwebp /tmp/$1-camsnap.jpg -quiet -m 6 -q 65 -psnr 37 -af -sharp_yuv -metadata none -o /tmp/$1-camsnap-optimised.webp

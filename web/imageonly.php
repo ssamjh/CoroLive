037da92adc67ad96de3e9ba5c7f46f11dec02de3
ssamjh <sam.howell.sjh@gmail.com>
2022-06-05 14:06:58 +1200

    Add random echo to end of URL on first load

--- web/imageonly.php
+++ web/imageonly.php
@@ -7,7 +7,7 @@ $camStill = "https://api.corolive.nz/$camera/snap.webp";
         <div class="col-xxl-8">
             <div class="ratio ratio-16x9">
                 <img id="image-only"
-                    src="<?php echo "$camStill"; ?>">
+                    src="<?php echo "$camStill"; ?>?=<?php echo (rand());?>">
             </div>
         </div>
     </div>

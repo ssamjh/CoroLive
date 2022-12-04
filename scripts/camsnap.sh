77ea6e6eef84e91872f0c9b7493cba2eb07abadb
ssamjh <sam.howell.sjh@gmail.com>
2022-12-04 23:05:08 +1300

    roll back

--- scripts/camsnap.sh
+++ scripts/camsnap.sh
@@ -1,2 +1,4 @@
-# Download and convert the image in a single command.
-curl --connect-timeout 2 --retry 4 --retry-delay 1 -s -S -f "$2" | cwebp -quiet -preset photo -q 50 -m 6 -resize 1280 720 -metadata none -o "/var/www/html/corolive.nz/api/$1/archive/latest/snap-$(date +%R).webp"
+# Grab a still from camera.
+curl --connect-timeout 2 --retry 4 --retry-delay 1 -s -S -f -o /tmp/$1-camsnap.jpg $2 \
+&& cwebp /tmp/$1-camsnap.jpg -quiet -preset photo -q 50 -resize 1280 720 -metadata none -o /tmp/$1-camsnap-optimised.webp \
+&& mv /tmp/$1-camsnap-optimised.webp "/var/www/html/corolive.nz/api/$1/archive/latest/snap-$(date +%R).webp"

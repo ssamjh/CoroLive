8dd8b4c0865413908328902925dd1148a020dfe2
ssamjh <sam.howell.sjh@gmail.com>
2022-12-04 20:50:02 +1300

    An AI suggested I do this to optimize my code

--- scripts/camanimation-p2.sh
+++ scripts/camanimation-p2.sh
@@ -1,9 +1,9 @@
-# Create the normal speed animation.
+# Create the normal speed animation. (crf was 28)
 cd /tmp/$1-animation/webp
-/opt/ffmpeg-git-20220108-amd64-static/ffmpeg -r 12 -i %d.webp -c:v libvpx-vp9 -b:v 0 -crf 38 -deadline good -cpu-used 5 -vf "fps=12" -pass 1 -an -f null /dev/null && \
-/opt/ffmpeg-git-20220108-amd64-static/ffmpeg -r 12 -i %d.webp -c:v libvpx-vp9 -b:v 0 -crf 38 -deadline good -cpu-used 5 -vf "fps=12" -pass 2 -an animation.webm
+ffmpeg -r 12 -i %d.webp -c:v libvpx-vp9 -b:v 0 -crf 38 -deadline good -cpu-used 5 -vf "fps=12,format=yuv420p" -pass 1 -an -f null /dev/null && \
+ffmpeg -r 12 -i %d.webp -c:v libvpx-vp9 -b:v 0 -crf 38 -deadline good -cpu-used 5 -vf "fps=12,format=yuv420p" -pass 2 -an animation.webm
 #
 # Create the faster speed animation.
 cd /tmp/$1-animation/webp
-/opt/ffmpeg-git-20220108-amd64-static/ffmpeg -r 30 -i %d.webp -c:v libvpx-vp9 -b:v 0 -crf 42 -deadline good -cpu-used 5 -vf "fps=30" -pass 1 -an -f null /dev/null && \
-/opt/ffmpeg-git-20220108-amd64-static/ffmpeg -r 30 -i %d.webp -c:v libvpx-vp9 -b:v 0 -crf 42 -deadline good -cpu-used 5 -vf "fps=30" -pass 2 -an animation-fast.webm
+ffmpeg -r 30 -i %d.webp -c:v libvpx-vp9 -b:v 0 -crf 42 -deadline good -cpu-used 5 -vf "fps=30,format=yuv420p" -pass 1 -an -f null /dev/null && \
+ffmpeg -r 30 -i %d.webp -c:v libvpx-vp9 -b:v 0 -crf 42 -deadline good -cpu-used 5 -vf "fps=30,format=yuv420p" -pass 2 -an animation-fast.webm

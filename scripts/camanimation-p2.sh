3ccd9f5cf33237a84c8ed99f1e4963c2c1b5a125
ssamjh <sam.howell.sjh@gmail.com>
2022-12-04 23:02:46 +1300

    More changes

--- scripts/camanimation-p2.sh
+++ scripts/camanimation-p2.sh
@@ -1,9 +1,18 @@
-# Create the normal speed animation. (crf was 28)
-cd /tmp/$1-animation/webp
-ffmpeg -r 12 -i %d.webp -c:v libvpx-vp9 -b:v 0 -crf 38 -deadline good -cpu-used 5 -vf "fps=12,format=yuv420p" -pass 1 -an -f null /dev/null && \
-ffmpeg -r 12 -i %d.webp -c:v libvpx-vp9 -b:v 0 -crf 38 -deadline good -cpu-used 5 -vf "fps=12,format=yuv420p" -pass 2 -an animation.webm
-#
-# Create the faster speed animation.
-cd /tmp/$1-animation/webp
-ffmpeg -r 30 -i %d.webp -c:v libvpx-vp9 -b:v 0 -crf 42 -deadline good -cpu-used 5 -vf "fps=30,format=yuv420p" -pass 1 -an -f null /dev/null && \
-ffmpeg -r 30 -i %d.webp -c:v libvpx-vp9 -b:v 0 -crf 42 -deadline good -cpu-used 5 -vf "fps=30,format=yuv420p" -pass 2 -an animation-fast.webm
+#!/bin/bash
+
+# Set the directory that contains the files to be animated
+input_dir="/tmp/$1-animation/webp"
+
+# Set the output file name and format
+output_file="/tmp/$1-animation/webp/animation.webm"
+
+# Set the fast output file name and format
+output_file_fast="/tmp/$1-animation/webp/animation-fast.webm"
+
+# Create the animation using ffmpeg in two-pass mode
+ffmpeg -r 12 -i "$input_dir/%d.webp" -c:v libvpx-vp9 -b:v 0 -crf 38 -deadline good -cpu-used 5 -vf "format=yuv420p" -pass 1 -an -f null /dev/null && \
+ffmpeg -r 12 -i "$input_dir/%d.webp" -c:v libvpx-vp9 -b:v 0 -crf 38 -deadline good -cpu-used 5 -vf "format=yuv420p" -pass 2 -an "$output_file"
+
+# Create the fast animation using ffmpeg in two-pass mode
+ffmpeg -r 30 -i "$input_dir/%d.webp" -c:v libvpx-vp9 -b:v 0 -crf 42 -deadline good -cpu-used 5 -vf "format=yuv420p" -pass 1 -an -f null /dev/null && \
+ffmpeg -r 30 -i "$input_dir/%d.webp" -c:v libvpx-vp9 -b:v 0 -crf 42 -deadline good -cpu-used 5 -vf "format=yuv420p" -pass 2 -an "$output_file_fast"
--- scripts/camanimation-p3.sh
+++ scripts/camanimation-p3.sh
@@ -1,5 +1,15 @@
-# Remove the old animation files.
-rm /var/www/html/corolive.nz/api/$1/{archive/latest,}/{animation.webm,animation-fast.webm}
-
-# Copy the new animations to today's folder and the api folder.
-cp /tmp/$1-animation/webp/*.webm /var/www/html/corolive.nz/api/$1/{archive/latest,}/
+# Cleanup the old animation files.
+rm /var/www/html/corolive.nz/api/$1/archive/latest/animation.webm
+rm /var/www/html/corolive.nz/api/$1/archive/latest/animation-fast.webm
+#
+# Copy the new animations to today's folder.
+cp /tmp/$1-animation/webp/animation.webm /var/www/html/corolive.nz/api/$1/archive/latest/animation.webm
+cp /tmp/$1-animation/webp/animation-fast.webm /var/www/html/corolive.nz/api/$1/archive/latest/animation-fast.webm
+#
+# Clean up the api animation files.
+rm /var/www/html/corolive.nz/api/$1/animation.webm
+rm /var/www/html/corolive.nz/api/$1/animation-fast.webm
+#
+# Copy the animations to the api folder.
+cp /tmp/$1-animation/webp/animation.webm /var/www/html/corolive.nz/api/$1/animation.webm
+cp /tmp/$1-animation/webp/animation-fast.webm /var/www/html/corolive.nz/api/$1/animation-fast.webm

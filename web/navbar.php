6bfd08aa44b4510133934aa1138e5c82366b8a79
ssamjh <sam.howell.sjh@gmail.com>
2023-03-15 22:20:38 +1300

    Don't allow direct access to lib files

--- web/navbar.php
+++ web/navbar.php
@@ -1,4 +1,20 @@
-<?php require 'local-security.php'; ?>
+<?php
+$backtrace = debug_backtrace();
+$direct_call = true;
+
+foreach ($backtrace as $trace) {
+    if (basename($trace['file']) !== basename(__FILE__)) {
+        $direct_call = false;
+        break;
+    }
+}
+
+if ($direct_call) {
+    header('HTTP/1.0 403 Forbidden');
+    echo 'You are not allowed to access this file directly.';
+    exit;
+}
+?>
 
 <nav class="navbar navbar-expand-sm navbar-dark bg-info border-bottom">
     <div class="container-fluid" id="navbar">

de547c889eab1ba916b3b2d46dee7dc52e6601a5
ssamjh <sam@sjh.at>
2024-05-06 15:14:35 +1200

    Fix library URL

--- web/head.php
+++ web/head.php
@@ -33,7 +33,7 @@ if ($direct_call) {
 <meta property="og:description"
     content="CoroLive is the Coromandel's live webcam network, with cameras in Whitianga, Whangamata and Thames plus extensive archives." />
 
-<script src=" https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.j "></script>
+<script src=" https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 

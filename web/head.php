e68478d6f5259c75894393bf52d289265604c096
ssamjh <sam@sjh.at>
2024-09-03 21:20:19 +1200

    Cleanup

--- web/head.php
+++ web/head.php
@@ -1,21 +1,3 @@
-<?php
-$backtrace = debug_backtrace();
-$direct_call = true;
-
-foreach ($backtrace as $trace) {
-    if (basename($trace['file']) !== basename(__FILE__)) {
-        $direct_call = false;
-        break;
-    }
-}
-
-if ($direct_call) {
-    header('HTTP/1.0 403 Forbidden');
-    echo 'You are not allowed to access this file directly.';
-    exit;
-}
-?>
-
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="HandheldFriendly" content="true">
--- web/navbar.php
+++ web/navbar.php
@@ -1,21 +1,3 @@
-<?php
-$backtrace = debug_backtrace();
-$direct_call = true;
-
-foreach ($backtrace as $trace) {
-    if (basename($trace['file']) !== basename(__FILE__)) {
-        $direct_call = false;
-        break;
-    }
-}
-
-if ($direct_call) {
-    header('HTTP/1.0 403 Forbidden');
-    echo 'You are not allowed to access this file directly.';
-    exit;
-}
-?>
-
 <nav class="navbar navbar-expand-sm navbar-dark bg-info border-bottom">
     <div class="container-fluid" id="navbar">
         <a class="navbar-brand" href="/">

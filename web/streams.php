3465f8ad6acffcc21e9d1d1a157c391d86aa7de8
ssamjh <sam.howell.sjh@gmail.com>
2026-06-24 14:10:27 +1200

    Swap back to API domain

--- web/streams.php
+++ web/streams.php
@@ -1,6 +1,6 @@
 <?php
 // Restreamer memfs stream IDs per camera.
-// Live stream: https://restreamer.corolive.nz/memfs/{id}.m3u8
+// Live stream: https://api.corolive.nz/memfs/{id}.m3u8
 $streamIds = [
     'whitianga' => '283795aa-816e-4d95-8ac0-05cabb67c05e',
     'whangamata' => 'd456ab62-df95-4872-a96a-734ff455996e',
@@ -10,5 +10,5 @@ $streamIds = [
 function streamM3u8($camera)
 {
     global $streamIds;
-    return "https://restreamer.corolive.nz/memfs/{$streamIds[$camera]}.m3u8";
+    return "https://api.corolive.nz/memfs/{$streamIds[$camera]}.m3u8";
 }
